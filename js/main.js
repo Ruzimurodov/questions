83d40674a6bc25f4569c6575d7ba3914dbf7f5e3
Ruzimurodov <ganisherruzimirodov@gmail.com>
2022-02-19 15:27:31 +0500

    Add js foulder

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// var firstName = "Hello";
+// var age = 21;
+// var student = "true";
+
+// console.log(firstName + "\n" + age + "\n" + student);
+
+// alert("Xush kelibsiz !");
+
+var firstName = prompt("Ismingizni kiriting");
+var oneQuestion = prompt("Table tegidagi td tegiga colspan='2' atributi berilsa brauzerda qanday ko'rinadi?");
+var twoQuestion = prompt("img-fluid klasi qanday vazifa bajaradi?");
+var threeQuestion = prompt("input-group klasi qanday vazifa bajaradi?");
+var fourQuestion = prompt("accordion ni ichida button tegi bo'ladimi?");
+var fiveQuestion = prompt("h1 ga text-muted klasi berilsa qanday o'zgarish bo'ladi?");
+var sixQuestion = prompt("Position elementlarini klasi bootstrapda qanday berilgan?Misol uchun fixed qanday beriladi?");
+var sevenQuestion = prompt("li teglaridagi nuqtalar va raqamlarni qaysi klass orqali olib tashlanadi?");
+var eightQuestion = prompt("btn-close klassli element qanaqa ko'rinishda bo'ladi?");
+var nineQuestion = prompt("h1 ga yozilgan matnni qatorni oxiriga o'tkazib qo'yish uchun qanaqa klass beriladi?");
+var tenQuestion = prompt("font-weight ni 700 qilish uchun bootstrapda qanaqa klass beriladi?");
+
+
+console.log(firstName + "\n" + oneQuestion + "\n" + twoQuestion + "\n" + threeQuestion + "\n" + fourQuestion + "\n" + fiveQuestion + "\n" + sixQuestion + "\n" + sevenQuestion + "\n" + eightQuestion + "\n" + nineQuestion + "\n" + tenQuestion);
+
